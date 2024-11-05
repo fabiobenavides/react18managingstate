e792cd474d7a908d980b3f68a13d3f68c67e9ca0
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2024-11-05 15:58:42 -0600

    useState and error boundary

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
+import { getProducts } from "./services/productService";
 
 export default function App() {
-  // function renderProduct(p) {
-  //   return (
-  //     <div key={p.id} className="product">
-  //       <a href="/">
-  //         <img src={`/images/${p.image}`} alt={p.name} />
-  //         <h3>{p.name}</h3>
-  //         <p>${p.price}</p>
-  //       </a>
-  //     </div>
-  //   );
-  // }
+
+  const [size, setSize] = useState("");
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    getProducts("shoes")
+      .then((Response) => setProducts(Response))
+      .catch((e) => setError(e));
+  }, []); // [] means the reason to re-render, empty means run only once
+
+  function renderProduct(p) {
+    return (
+      <div key={p.id} className="product">
+        <a href="/">
+          <img src={`/images/${p.image}`} alt={p.name} />
+          <h3>{p.name}</h3>
+          <p>${p.price}</p>
+        </a>
+      </div>
+    );
+  }
+
+  const filteredProducts = size 
+    ? products.filter(pro => pro.skus.find(s => s.size === parseInt(size)))
+    : products;
+
+  if (error) throw error;
 
   return (
     <>
@@ -23,12 +41,16 @@ export default function App() {
         <main>
           <section id="filters">
             <label htmlFor="size">Filter by Size:</label>{" "}
-            <select id="size">
+            <select id="size" value={size} onChange={(e) => setSize(e.target.value)}>
               <option value="">All sizes</option>
               <option value="7">7</option>
               <option value="8">8</option>
               <option value="9">9</option>
             </select>
+          </section>
+          {size && <h2>Found {filteredProducts.length} items</h2>}
+          <section id="products">
+            {filteredProducts.map(renderProduct)}
           </section>
         </main>
       </div>
