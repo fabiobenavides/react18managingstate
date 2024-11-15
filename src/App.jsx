499d86bc9b219cc08874cef32bf03e2829465005
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2024-11-15 14:50:52 -0600

    Form state and validation

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Products from "./Products";
 import { Routes, Route } from "react-router-dom"
 import Detail from "./Detail";
 import Cart from "./Cart";
+import Checkout from "./Checkout";
 
 export default function App() {
 
@@ -53,6 +54,10 @@ export default function App() {
     )
   }
 
+  function emptyCart() {
+    setCart([]);
+  }
+
   return (
     <>
       <div className="content">
@@ -63,6 +68,7 @@ export default function App() {
             <Route path="/:category" element={<Products />} />
             <Route path="/:category/:id" element={<Detail addToCart={addToCart} />} />
             <Route path="/cart" element={<Cart cart={cart} updateQuantity={updateQuantity} />} />
+            <Route path="/checkout" element={<Checkout cart={cart} emptyCart={emptyCart} />} />
           </Routes>
         </main>
       </div>
--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import useFetchAll from "./services/useFetchAll";
 import Spinner from "./Spinner";
+import { useNavigate } from "react-router-dom";
 
 export default function Cart({ cart, updateQuantity }) {
+
+  const navigate = useNavigate();
   const urls = cart.map((i) => `products/${i.id}`);
   const { data: products, loading, error } = useFetchAll(urls);
 
@@ -47,6 +50,9 @@ export default function Cart({ cart, updateQuantity }) {
     <section id="cart">
       <h1>Cart</h1>
       <ul>{cart.map(renderItem)}</ul>
+      <button className="btn btn-primary" onClick={() => navigate("/checkout")} >
+        Checkout
+      </button>
     </section>
   );
 }
